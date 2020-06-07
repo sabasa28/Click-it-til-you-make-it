5c266fc7778ecd39c1fa0f24134b3747229aee37
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-06-07 00:01:22 -0300

    Falle totalmente en mantener el juego en diferentes aspect ratios

---
+++ Click it til you make it IDG/Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public void LoadGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Debug.Break();
+        Debug.Log("Game Closed");
+    }
+}
---
+++ Click it til you make it IDG/Assets/Scripts/UIPause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIPause : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Debug.Break();
+        Debug.Log("Game Closed");
+    }
+    public void ReturnToGameplayScene()
+    {
+
+    }
+}
