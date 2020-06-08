e60bd37d59ca40ccba034b9ab1afdb915ba3c4e2
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-06-07 23:34:23 -0300

    Avanzada base para la UI

---
+++ Click it til you make it IDG/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public float petsPerClick = 1;
+    float maxPetsReached = 0;
+    float currentPets = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateMaxPets(int currentPetAmmount)
+    {
+        if (currentPetAmmount>maxPetsReached) maxPetsReached = currentPetAmmount;
+    }
+
+    void OnClickAddPets()
+    {
+        currentPets += petsPerClick;
+    }
+
+}
